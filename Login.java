9b562ffc22ff26df728c7b625c163b85ad68f12e
Ravikumar <ravikumar@gmail.com>
2019-03-26 10:03:48 +0000

    Changes made to Login

--- Login.java
+++ Login.java
@@ -3,6 +3,7 @@ class Login
 public static void main (String args[])
 {
 System.out.println("My first Login to Git");
+System.out.println("My second clone");
 System.out.println("Clone is done");
 }
 }
