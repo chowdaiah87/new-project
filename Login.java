27900838fae6a72a6b0f57bced3c1a073e57c169
Ravikumar <ravikumar@gmail.com>
2019-03-30 09:42:42 +0000

    Print statement removed

--- Login.java
+++ Login.java
@@ -4,7 +4,6 @@ public static void main (String args[])
 {
 System.out.println("My first Login to Git");
 System.out.println("My second clone");
-System.out.println("My Print statement");
 System.out.println("Clone is done");
 System.out.println("New changes");
 }
